ba4f50023382fac7f502e9907b5ce9019b8c8a01
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2024-11-04 11:03:28 +0700

    feat(model): identities

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -3,8 +3,9 @@ import { Elysia } from "elysia";
 
 // import routes
 import UserRoutes from "./UserRoutes";
+import IdentityRoutes from "./IdentityRoutes";
 
-const Routes = new Elysia().use(UserRoutes);
+const Routes = new Elysia().use(UserRoutes).use(IdentityRoutes);
 
 // Export Routes sebagai default
 export default Routes;
