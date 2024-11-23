0cb2fe2b8d74c0479dcfc4a2b05292392fb09c20
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2024-11-23 18:50:58 +0700

    feat: implement jwt in auth

--- backend/src/routes/AuthRoutes.ts
+++ backend/src/routes/AuthRoutes.ts
@@ -1,16 +1,31 @@
 import { Elysia } from "elysia";
 import { login } from "../controllers/AuthControllers";
+import jwt from "@elysiajs/jwt";
 
-const AuthRoutes = new Elysia({ prefix: "/auth" }).post(
-    "/login",
-    async ({ body }) => {
+const AuthRoutes = new Elysia({ prefix: "/auth" })
+    .use(
+        jwt({
+            name: "jwt",
+            secret: "Skibidi_Sigma_Mewing_Rizz_Gyatt_inOhio_+1000Aura",
+            exp: "1d",
+        })
+    )
+    .use(
+        jwt({
+            name: "jwt_refresh",
+            secret: "Skibidi_Sigma_Mewing_Rizz_Gyatt_inOhio_+1000Aura",
+            exp: "1m",
+        })
+    )
+    .post("/login", async ({ jwt, jwt_refresh, body }) => {
         return await login(
             body as {
                 username: string;
                 password: string;
-            }
+            },
+            jwt,
+            jwt_refresh
         );
-    }
-);
+    });
 
 export default AuthRoutes;
