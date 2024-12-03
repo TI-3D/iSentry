c2ffff5b59dd6b0b690726e7c36b694ed7e1bf19
Yoanvari <yoanvari123@gmail.com>
2024-12-03 10:42:00 +0700

    feat: add detection bloc(GetDetectionById, GetAllDetection, DeleteDetection)

---
+++ mobile/lib/presentation/home/bloc/detection_log/detection_state.dart
@@ -0,0 +1,43 @@
+import 'package:equatable/equatable.dart';
+import 'package:isentry/data/models/detection_model.dart';
+import 'package:isentry/domain/entities/detection.dart';
+
+abstract class DetectionState extends Equatable {
+  @override
+  List<Object?> get props => [];
+}
+
+class DetectionInitial extends DetectionState {}
+
+class DetectionLoading extends DetectionState {}
+
+class DetectionLoaded extends DetectionState {
+  final Detection detection;
+
+  DetectionLoaded(this.detection);
+
+  @override
+  List<Object?> get props => [detection];
+}
+
+class DetectionFailure extends DetectionState {
+  final String errorMessage;
+
+  DetectionFailure(this.errorMessage);
+
+  @override
+  List<Object?> get props => [errorMessage];
+}
+
+class AllDetectionLoaded extends DetectionState {
+  final List<DetectionModel> detections;
+
+  AllDetectionLoaded(this.detections);
+
+  @override
+  List<Object?> get props => [detections];
+}
+
+class DetectionDeleted extends DetectionState {}
+
+class DetectionSuccess extends DetectionState {}
