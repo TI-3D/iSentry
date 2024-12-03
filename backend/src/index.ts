9a6eb9d468e30951da00168980b871e6340ea826
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2024-12-03 12:49:50 +0700

    fix: staticplugin routes

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,17 +1,15 @@
 import { Elysia } from "elysia";
 import Routes from "./routes";
 import swagger from "@elysiajs/swagger";
+import { staticPlugin } from "@elysiajs/static";
 
 const app = new Elysia();
-
+app.use(staticPlugin({ assets: "assets", prefix: "/public" }));
 app.use(swagger());
 app.get("/", () => "Hello Elysia!");
-app.group("/api", (app) =>
-    app
-        .use(Routes)
-);
+app.group("/api", (app) => app.use(Routes));
 app.listen({
-    hostname: "0.0.0.0", // change sesuai IP address
+    hostname: "localhost", // change sesuai IP address
     port: 3000,
 });
 
