3b11bc3e22533fbfaf63d9ffe4d3779091f85d54
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2024-12-03 14:33:20 +0700

    fix: add created at on face

--- backend/src/controllers/FaceController.ts
+++ backend/src/controllers/FaceController.ts
@@ -25,6 +25,7 @@ export async function getFace() {
                         path: true,
                     },
                 },
+                createdAt: true,
             },
         });
         for (const item of faces) {
