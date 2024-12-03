6e48f68065e4f914c20ef7924ec43bcb92907393
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2024-12-03 13:26:38 +0700

    feat: routes detail detection log

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -44,8 +44,6 @@ export async function createUser(options: {
             };
         }
         const hashedPassword = await Bun.password.hash(options.password);
-
-        const hashedPassword = await Bun.password.hash(options.password);
         const users = await prisma.user.create({
             data: {
                 username: options.username,
