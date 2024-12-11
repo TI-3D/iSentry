936001a2aed700927fb06e9152d6dd6e8189e18f
Yoanvari <yoanvari123@gmail.com>
2024-12-11 17:20:48 +0700

    fix: logic logout

--- mobile/lib/presentation/home/pages/profile/account_settings.dart
+++ mobile/lib/presentation/home/pages/profile/account_settings.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:isentry/common/helper/navigation/app_navigation.dart';
+import 'package:isentry/presentation/auth/bloc/login_bloc.dart';
+import 'package:isentry/presentation/auth/bloc/login_event.dart';
+import 'package:isentry/presentation/auth/bloc/login_state.dart';
 import 'package:isentry/presentation/auth/pages/login.dart';
 import 'package:isentry/presentation/home/pages/profile/qr_code.dart';
 import 'package:isentry/presentation/widgets/appBar/appbar.dart';
-import 'package:isentry/services/secure_storage_service.dart';
 import 'package:lucide_icons/lucide_icons.dart';
 
 class AccountSettingsPage extends StatelessWidget {
@@ -12,203 +15,216 @@ class AccountSettingsPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: CustomAppBar(
-        title: 'Account Settings',
-        onLeadingPressed: () {
-          Navigator.pop(context);
-        },
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Profile Header
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const Row(
-                  children: [
-                    CircleAvatar(
-                      radius: 30,
-                      backgroundColor: Colors.grey,
-                      child: Text('I'),
-                    ),
-                    SizedBox(width: 10),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        Text(
-                          'Hello!',
-                          style: TextStyle(
-                              fontSize: 14,
-                              fontWeight: FontWeight.w600,
-                              letterSpacing: 1.2,
-                              color: Colors.grey),
-                        ),
-                        Text(
-                          'userId',
-                          style: TextStyle(
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                            letterSpacing: 1.2,
+    return BlocListener<LoginBloc, LoginState>(
+      listener: (context, state) {
+        if (state is LogoutSuccess) {
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+            content: Text('Logout Success'),
+            duration: Duration(seconds: 2),
+          ));
+          AppNavigator.push(context, const LoginPage());
+        } else if (state is LoginFailure) {
+          ScaffoldMessenger.of(context)
+              .showSnackBar(SnackBar(content: Text(state.errorMessage)));
+        }
+      },
+      child: Scaffold(
+        appBar: CustomAppBar(
+          title: 'Account Settings',
+          onLeadingPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Profile Header
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Row(
+                    children: [
+                      CircleAvatar(
+                        radius: 30,
+                        backgroundColor: Colors.grey,
+                        child: Text('I'),
+                      ),
+                      SizedBox(width: 10),
+                      Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Hello!',
+                            style: TextStyle(
+                                fontSize: 14,
+                                fontWeight: FontWeight.w600,
+                                letterSpacing: 1.2,
+                                color: Colors.grey),
                           ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                IconButton(
-                  icon: const Icon(LucideIcons.qrCode, size: 26),
-                  onPressed: () {
-                    AppNavigator.push(context, QrCodePage(userId: userId));
-                  },
-                ),
-              ],
-            ),
-            const SizedBox(height: 15),
+                          Text(
+                            'userId',
+                            style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              letterSpacing: 1.2,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    icon: const Icon(LucideIcons.qrCode, size: 26),
+                    onPressed: () {
+                      AppNavigator.push(context, QrCodePage(userId: userId));
+                    },
+                  ),
+                ],
+              ),
+              const SizedBox(height: 15),
 
-            // Menu Items
-            ListTile(
-              dense: true,
-              leading: const Icon(LucideIcons.userCircle2),
-              title: const Text(
-                'Edit Account',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 15,
-                ),
-              ),
-              subtitle: const Text(
-                'Update your personal information.',
-                style: TextStyle(
-                    fontSize: 13,
-                    color: Colors.grey,
+              // Menu Items
+              ListTile(
+                dense: true,
+                leading: const Icon(LucideIcons.userCircle2),
+                title: const Text(
+                  'Edit Account',
+                  style: TextStyle(
                     fontWeight: FontWeight.w500,
-                    letterSpacing: 1),
-              ),
-              onTap: () {
-                // Logic for Edit Account
-              },
-            ),
-            const Divider(),
-            ListTile(
-              dense: true,
-              leading: const Icon(LucideIcons.history),
-              title: const Text(
-                'Log Activity',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 15,
+                    fontSize: 15,
+                  ),
                 ),
-              ),
-              subtitle: const Text(
-                'View your activity history.',
-                style: TextStyle(
-                    fontSize: 13,
-                    color: Colors.grey,
-                    fontWeight: FontWeight.w500,
-                    letterSpacing: 1),
-              ),
-              onTap: () {
-                // Logic for Log Activity
-              },
-            ),
-            const Divider(),
-            // // ListTile(
-            // //   leading: const Icon(LucideIcons.settings),
-            // //   title: const Text('Setting',
-            // //     style: TextStyle(
-            // //       fontWeight: FontWeight.w500,
-            // //       fontSize: 15,
-            // //     ),
-            // //   ),
-            // //   subtitle: const Text('Customize your app experience.',
-            // //     style: TextStyle(
-            // //       fontSize: 13,
-            // //       color: Colors.grey,
-            // //       fontWeight: FontWeight.w500,
-            // //       letterSpacing: 1
-            // //     ),
-            // //   ),
-            // //   onTap: () {
-            // //     // Logic for Settings
-            // //   },
-            // // ),
-            // const Divider(),
-            ListTile(
-              dense: true,
-              leading: const Icon(LucideIcons.settings),
-              title: const Text(
-                'Setting',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 15,
+                subtitle: const Text(
+                  'Update your personal information.',
+                  style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1),
                 ),
+                onTap: () {
+                  // Logic for Edit Account
+                },
               ),
-              subtitle: const Text(
-                'Customize your app experience.',
-                style: TextStyle(
-                    fontSize: 13,
-                    color: Colors.grey,
+              const Divider(),
+              ListTile(
+                dense: true,
+                leading: const Icon(LucideIcons.history),
+                title: const Text(
+                  'Log Activity',
+                  style: TextStyle(
                     fontWeight: FontWeight.w500,
-                    letterSpacing: 1),
-              ),
-              onTap: () {
-                // Logic for Settings
-              },
-            ),
-            const Divider(),
-            ListTile(
-              leading: const Icon(LucideIcons.heartHandshake),
-              title: const Text(
-                'About Us',
-                style: TextStyle(
-                  fontWeight: FontWeight.w500,
-                  fontSize: 15,
+                    fontSize: 15,
+                  ),
                 ),
+                subtitle: const Text(
+                  'View your activity history.',
+                  style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1),
+                ),
+                onTap: () {
+                  // Logic for Log Activity
+                },
               ),
-              subtitle: const Text(
-                'Learn app and development team.',
-                style: TextStyle(
-                    fontSize: 13,
-                    color: Colors.grey,
+              const Divider(),
+              // // ListTile(
+              // //   leading: const Icon(LucideIcons.settings),
+              // //   title: const Text('Setting',
+              // //     style: TextStyle(
+              // //       fontWeight: FontWeight.w500,
+              // //       fontSize: 15,
+              // //     ),
+              // //   ),
+              // //   subtitle: const Text('Customize your app experience.',
+              // //     style: TextStyle(
+              // //       fontSize: 13,
+              // //       color: Colors.grey,
+              // //       fontWeight: FontWeight.w500,
+              // //       letterSpacing: 1
+              // //     ),
+              // //   ),
+              // //   onTap: () {
+              // //     // Logic for Settings
+              // //   },
+              // // ),
+              // const Divider(),
+              ListTile(
+                dense: true,
+                leading: const Icon(LucideIcons.settings),
+                title: const Text(
+                  'Setting',
+                  style: TextStyle(
                     fontWeight: FontWeight.w500,
-                    letterSpacing: 1),
+                    fontSize: 15,
+                  ),
+                ),
+                subtitle: const Text(
+                  'Customize your app experience.',
+                  style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1),
+                ),
+                onTap: () {
+                  // Logic for Settings
+                },
               ),
-              onTap: () {
-                // Logic for About Us
-              },
-            ),
-            const Divider(),
+              const Divider(),
+              ListTile(
+                leading: const Icon(LucideIcons.heartHandshake),
+                title: const Text(
+                  'About Us',
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    fontSize: 15,
+                  ),
+                ),
+                subtitle: const Text(
+                  'Learn app and development team.',
+                  style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1),
+                ),
+                onTap: () {
+                  // Logic for About Us
+                },
+              ),
+              const Divider(),
 
-            // Logout
-            ListTile(
-              dense: true,
-              leading: const Icon(LucideIcons.logOut, color: Colors.red),
-              title: const Text(
-                'Log Out',
-                style: TextStyle(
-                  color: Colors.red,
-                  fontWeight: FontWeight.w500,
-                  fontSize: 15,
-                ),
-              ),
-              subtitle: const Text(
-                'Log out of your account.',
-                style: TextStyle(
-                    fontSize: 13,
-                    color: Colors.grey,
+              // Logout
+              ListTile(
+                dense: true,
+                leading: const Icon(LucideIcons.logOut, color: Colors.red),
+                title: const Text(
+                  'Log Out',
+                  style: TextStyle(
+                    color: Colors.red,
                     fontWeight: FontWeight.w500,
-                    letterSpacing: 1),
+                    fontSize: 15,
+                  ),
+                ),
+                subtitle: const Text(
+                  'Log out of your account.',
+                  style: TextStyle(
+                      fontSize: 13,
+                      color: Colors.grey,
+                      fontWeight: FontWeight.w500,
+                      letterSpacing: 1),
+                ),
+                onTap: () {
+                  context.read<LoginBloc>().add(LogoutRequested(id: userId));
+                },
               ),
-              onTap: () {
-                SecureStorageService.deleteAll();
-                AppNavigator.pushAndRemove(context, const LoginPage());
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
