14b8d03a8f30897f48ee49876bb24b10fc5b5fe6
Byan Nur Cahyo <byannurcahyo9@gmail.com>
2024-12-16 13:19:59 +0700

    fix: identity schema and changes seeder

--- backend/src/controllers/IdentityController.ts
+++ backend/src/controllers/IdentityController.ts
@@ -150,6 +150,7 @@ export async function getIdentityById(id: string) {
                         },
                     },
                 },
+                key: true,
             },
         });
 
